2303bf92cc785c6d851f669d4f904cba25cae689
Kelli Garner <32491649+Kellibelli@users.noreply.github.com>
2017-11-13 19:39:11 -0700

    Update PatientArray.java

--- src/arrayClassesPackage/PatientArray.java
+++ src/arrayClassesPackage/PatientArray.java
@@ -2,71 +2,44 @@ package arrayClassesPackage;
 
 public class PatientArray {
 
-	public Patient[] myPatients = new Patient[10];
+	public Patient[] myPatients = new Patient[2];
 
 	public void buildArray() {
 
 		myPatients[0] = new Patient();
-		myPatients[0].getBreed();
-		myPatients[0].getGender();
-		myPatients[0].getSymptoms();
+		myPatients[0].setBreed(null);
+		myPatients[0].setGender(null);
+		myPatients[0].setSymptoms(null);
+		myPatients[0].setAge(null);
 		System.out.println("");
 		myPatients[1] = new Patient();
-		myPatients[1].getBreed();
-		myPatients[1].getGender();
-		myPatients[1].getSymptoms();
+		myPatients[1].setBreed(null);
+		myPatients[1].setGender(null);
+		myPatients[1].setSymptoms(null);
+		myPatients[1].setAge(null);
 		System.out.println("");
-		myPatients[1] = new Patient();
-		myPatients[1].getBreed();
-		myPatients[1].getGender();
-		myPatients[1].getSymptoms();
+		// myPatients[1] = new Patient();
+		// myPatients[1].setBreed(null);
+		// myPatients[1].setGender(null);
+		// myPatients[1].setSymptoms(null);
 		System.out.println("");
-		myPatients[2] = new Patient();
-		myPatients[2].getBreed();
-		myPatients[2].getGender();
-		myPatients[2].getSymptoms();
+		// myPatients[2] = new Patient();
+		// myPatients[2].setBreed(null);
+		// myPatients[2].setGender(null);
+		// myPatients[2].setSymptoms(null);
 		System.out.println("");
-		myPatients[3] = new Patient();
-		myPatients[3].getBreed();
-		myPatients[3].getGender();
-		myPatients[3].getSymptoms();
-		System.out.println("");
-		myPatients[4] = new Patient();
-		myPatients[4].getBreed();
-		myPatients[4].getGender();
-		myPatients[4].getSymptoms();
-		System.out.println("");
-		myPatients[5] = new Patient();
-		myPatients[5].getBreed();
-		myPatients[5].getGender();
-		myPatients[5].getSymptoms();
-		System.out.println("");
-		myPatients[6] = new Patient();
-		myPatients[6].getBreed();
-		myPatients[6].getGender();
-		myPatients[6].getSymptoms();
-		System.out.println("");
-		myPatients[0] = new Patient();
-		myPatients[0].getBreed();
-		myPatients[0].getGender();
-		myPatients[0].getSymptoms();
-		System.out.println("");
-		myPatients[7] = new Patient();
-		myPatients[7].getBreed();
-		myPatients[7].getGender();
-		myPatients[7].getSymptoms();
-		System.out.println("");
-		myPatients[8] = new Patient();
-		myPatients[8].getBreed();
-		myPatients[8].getGender();
-		myPatients[8].getSymptoms();
-		System.out.println("");
-		myPatients[9] = new Patient();
-		myPatients[9].getBreed();
-		myPatients[9].getGender();
-		myPatients[9].getSymptoms();
-		System.out.println("");
-		
+
+	}
+
+	public void showArray() {
+		for (int s = 0; s < myPatients.length; s++) {
+			System.out.println("Breed: " + myPatients[s].getBreed());
+			System.out.println("Gender: " + myPatients[s].getGender());
+			System.out.println("Symptoms: " + myPatients[s].getSymptoms());
+			System.out.println("Age: " + myPatients[s].getAge());
+			System.out.println("");
+		}
+
 	}
 
 }
